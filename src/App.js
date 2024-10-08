4c79097c577ba1039c6db844de736995a6a740c9
KaiO277 <nghiatran1527@gmail.com>
2024-10-08 21:30:11 +0700

    Two-way binding_KaiO

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import { useState } from 'react'
 
-const gifts = [
-  'CPU',
-  'IPHONE',
-  'MOTO'
-]
 
 function App() {
 
-  const [gift, setGift] = useState()
+  const [name, setName] = useState('')
+  console.log(name);
+  
 
   const handleButton = () =>{
-    setGift(gifts[Math.floor(Math.random()*gifts.length)])
+    
   }
 
   return (
     <div className="App">
       <h1>Bai 1:</h1>
-      <h3>{gift||'Chua co phan thuong'}</h3>
-      <button onClick={handleButton}>Click</button>
+      <input 
+        value={name}
+        onChange={e=>setName(e.target.value)}
+      />
+      <button onClick={()=>setName("Tran Van Nghia")}>Click</button>
     </div>
   );
 }
